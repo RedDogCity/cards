45d0d957e73af44ee20cc30cae8514378f03c84a
DilanIsHere <dhamidworks13@gmail.com>
2025-03-12 15:12:27 -0400

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
+import NavigationBar from './components/NavigationBar.tsx';
+import HomePage from './pages/HomePage.tsx'
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
+      <NavigationBar />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/cards" element={<CardPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterPage />}
+        />
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
