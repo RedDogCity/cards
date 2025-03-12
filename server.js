10702bf457409df5582c5e097702200ff7e348da
DilanIsHere <dhamidworks13@gmail.com>
2025-03-12 15:13:11 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,255 +1,65 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://acvalcon:mangopie123@cop4331.bzadf.mongodb.net/?retryWrites=true&w=majority&appName=COP4331";
-
-const client = new MongoClient(url);
-client.connect();
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+//require('dotenv').config();
+//const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://root:WhatAPassword@test.2svvr.mongodb.net/?retryWrites=true&w=majority&appName=Test';
+const client = new MongoClient(url);
+client.connect();
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-var cardList =
-    [
-        'Roy Campanella',
-        'Paul Molitor',
-        'Tony Gwynn',
-        'Dennis Eckersley',
-        'Reggie Jackson',
-        'Gaylord Perry',
-        'Buck Leonard',
-        'Rollie Fingers',
-        'Charlie Gehringer',
-        'Wade Boggs',
-        'Carl Hubbell',
-        'Dave Winfield',
-        'Jackie Robinson',
-        'Ken Griffey, Jr.',
-        'Al Simmons',
-        'Chuck Klein',
-        'Mel Ott',
-        'Mark McGwire',
-        'Nolan Ryan',
-        'Ralph Kiner',
-        'Yogi Berra',
-        'Goose Goslin',
-        'Greg Maddux',
-        'Frankie Frisch',
-        'Ernie Banks',
-        'Ozzie Smith',
-        'Hank Greenberg',
-        'Kirby Puckett',
-        'Bob Feller',
-        'Dizzy Dean',
-        'Joe Jackson',
-        'Sam Crawford',
-        'Barry Bonds',
-        'Duke Snider',
-        'George Sisler',
-        'Ed Walsh',
-        'Tom Seaver',
-        'Willie Stargell',
-        'Bob Gibson',
-        'Brooks Robinson',
-        'Steve Carlton',
-        'Joe Medwick',
-        'Nap Lajoie',
-        'Cal Ripken, Jr.',
-        'Mike Schmidt',
-        'Eddie Murray',
-        'Tris Speaker',
-        'Al Kaline',
-        'Sandy Koufax',
-        'Willie Keeler',
-        'Pete Rose',
-        'Robin Roberts',
-        'Eddie Collins',
-        'Lefty Gomez',
-        'Lefty Grove',
-        'Carl Yastrzemski',
-        'Frank Robinson',
-        'Juan Marichal',
-        'Warren Spahn',
-        'Pie Traynor',
-        'Roberto Clemente',
-        'Harmon Killebrew',
-        'Satchel Paige',
-        'Eddie Plank',
-        'Josh Gibson',
-        'Oscar Charleston',
-        'Mickey Mantle',
-        'Cool Papa Bell',
-        'Johnny Bench',
-        'Mickey Cochrane',
-        'Jimmie Foxx',
-        'Jim Palmer',
-        'Cy Young',
-        'Eddie Mathews',
-        'Honus Wagner',
-        'Paul Waner',
-        'Grover Alexander',
-        'Yogi Berra',
-        'Goose Goslin',
-        'Greg Maddux',
-        'Frankie Frisch',
-        'Ernie Banks',
-        'Ozzie Smith',
-        'Hank Greenberg',
-        'Kirby Puckett',
-        'Bob Feller',
-        'Dizzy Dean',
-        'Joe Jackson',
-        'Sam Crawford',
-        'Barry Bonds',
-        'Duke Snider',
-        'George Sisler',
-        'Ed Walsh',
-        'Tom Seaver',
-        'Willie Stargell',
-        'Bob Gibson',
-        'Brooks Robinson',
-        'Steve Carlton',
-        'Joe Medwick',
-        'Nap Lajoie',
-        'Cal Ripken, Jr.',
-        'Mike Schmidt',
-        'Eddie Murray',
-        'Tris Speaker',
-        'Al Kaline',
-        'Sandy Koufax',
-        'Willie Keeler',
-        'Pete Rose',
-        'Robin Roberts',
-        'Eddie Collins',
-        'Lefty Gomez',
-        'Lefty Grove',
-        'Carl Yastrzemski',
-        'Frank Robinson',
-        'Juan Marichal',
-        'Warren Spahn',
-        'Pie Traynor',
-        'Roberto Clemente',
-        'Harmon Killebrew',
-        'Satchel Paige',
-        'Eddie Plank',
-        'Josh Gibson',
-        'Oscar Charleston',
-        'Mickey Mantle',
-        'Cool Papa Bell',
-        'Johnny Bench',
-        'Mickey Cochrane',
-        'Jimmie Foxx',
-        'Jim Palmer',
-        'Cy Young',
-        'Eddie Mathews',
-        'Honus Wagner',
-        'Paul Waner',
-        'Grover Alexander',
-        'Rod Carew',
-        'Joe DiMaggio',
-        'Joe Morgan',
-        'Stan Musial',
-        'Bill Terry',
-        'Rogers Hornsby',
-        'Lou Brock',
-        'Ted Williams',
-        'Bill Dickey',
-        'Christy Mathewson',
-        'Willie McCovey',
-        'Lou Gehrig',
-        'George Brett',
-        'Hank Aaron',
-        'Harry Heilmann',
-        'Walter Johnson',
-        'Roger Clemens',
-        'Ty Cobb',
-        'Whitey Ford',
-        'Willie Mays',
-        'Rickey Henderson',
-        'Babe Ruth'
-    ];
-
-app.post('/api/addcard', async (req, res, next) => {
-    // incoming: userId, color
-    // outgoing: error
-
-    const { userId, card } = req.body;
-
-    const newCard = { Card: card, UserId: userId };
-    var error = '';
-
-    try {
-        const db = client.db();
-        const result = db.collection('Cards').insertOne(newCard);
-    }
-    catch (e) {
-        error = e.toString();
-    }
-
-    cardList.push(card);
-
-    var ret = { error: error };
-    res.status(200).json(ret); // changed from .tsxon to .json
-});
-app.post('/api/login', async (req, res, next) => {
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { login, password } = req.body;
-    const db = client.db();
-    const results = await
-        db.collection('Users').find({ Login: login, Password: password }).toArray();
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    if (results.length > 0) {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-    }
-    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
-    res.status(200).json(ret); // changed from .tsxon to .json
-});
-
-
-
-
-app.post('/api/searchcards', async (req, res, next) => {
-    // incoming: userId, search
-    // outgoing: results[], error
-
-    var error = '';
-
-    const { userId, search } = req.body;
-
-    var _search = search.trim();
-
-    const db = client.db();
-    const results = await db.collection('Cards').find({ "Card": { $regex: _search + '.*', $options: 'r' } }).toArray();
-
-    var _ret = [];
-    for (var i = 0; i < results.length; i++) {
-        _ret.push(results[i].Card);
-    }
-
-    var ret = { results: _ret, error: error };
-    res.status(200).json(ret); // changed from .tsxon to .json
-});
-
-
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
+    res.setHeader('Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS');
     next();
+}
+);
+
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, name, emailAddress, error
+
+    var error = '';
+
+    const { login, password } = req.body;
+
+    const db = client.db('COP4331');
+    const results = await db.collection('Users').find({login:login, password:password}).toArray();
+
+    var id = -1;
+    var name = '';
+    var email = '';
+
+    if( results.length > 0 )
+    {
+        id = results[0]._id;
+        name = results[0].name;
+        email = results[0].emailAddress;
+    }
+
+    var ret = { id: id, name: name, emailAdress: email, error: error};
+    res.status(200).json(ret);
 });
-app.listen(5000); // start Node + Express server on port 5000
+
+app.post('/api/register', async (req, res, next) => {
+    // incoming: login, password, name, emailAddress
+    // outgoing: error
+
+    var error = '';
+
+    const db = client.db('COP4331');
+    const results = await db.collection('Users').insertOne(req.body);
+
+    var ret = { error: error };
+    res.status(200).json(ret);
+});
+
+app.listen(5000);
