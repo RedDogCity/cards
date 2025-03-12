5eb25d3102093e6cee94689bee6c90869404bd61
DilanIsHere <dhamidworks13@gmail.com>
2025-03-12 15:11:40 -0400

    Add files via upload

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import Greeter from "../components/Greeter.tsx";
+
+const HomePage = () =>
+{
+    return(
+        <div>
+            <Greeter />
+        </div>
+    );
+};
+
+export default HomePage;
